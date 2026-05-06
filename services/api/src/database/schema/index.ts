c449cf2b5b57b22313ea3b614f4dd88a4dcffa6f
idenn207 <skypark207@gmail.com>
2026-05-06 21:40:06 +0900

    feat: folders 테이블 스키마 추가

--- services/api/src/database/schema/index.ts
+++ services/api/src/database/schema/index.ts
@@ -1,5 +1,6 @@
 export * from './backup-codes.schema';
 export * from './devices.schema';
+export * from './folders.schema';
 export * from './invitations.schema';
 export * from './permissions.schema';
 export * from './refresh-tokens.schema';
