4809e2c35667821c75fd62b2e15770b888e293b3
idenn207 <skypark207@gmail.com>
2026-05-07 04:28:50 +0900

    feat: TransactionContext 구현 및 DatabaseModule 등록

--- services/api/src/database/index.ts
+++ services/api/src/database/index.ts
@@ -1,3 +1,4 @@
 export * from './database.module';
 export * from './database.service';
 export * from './schema';
+export * from './transaction-context';
---
+++ services/api/src/database/transaction-context.ts
@@ -0,0 +1,14 @@
+import { AsyncLocalStorage } from 'node:async_hooks';
+import { DrizzleTx } from './types/database.type';
+
+export class TransactionContext {
+  private readonly storage = new AsyncLocalStorage<DrizzleTx>();
+
+  get current(): DrizzleTx | undefined {
+    return this.storage.getStore();
+  }
+
+  run<T>(tx: DrizzleTx, fn: () => Promise<T>): Promise<T> {
+    return this.storage.run(tx, fn);
+  }
+}
