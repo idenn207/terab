d31930caeb7299d3b7852fde78663b6ff6289f55
idenn207 <skypark207@gmail.com>
2026-05-13 20:14:28 +0900

    refactor(api): FileModule에 UploadSession providers 등록

--- services/api/src/file/file.module.ts
+++ services/api/src/file/file.module.ts
@@ -1,28 +1,18 @@
+import { BullModule } from '@nestjs/bullmq';
 import { Module } from '@nestjs/common';
-import { MulterModule } from '@nestjs/platform-express';
 import { FolderModule } from '../folder/folder.module';
-import { MinioStorageEngine } from '../minio/minio-storage.engine';
-import { MinioService } from '../minio/minio.service';
 import { FileDownloadController } from './file-download.controller';
+import { FileUploadController } from './file-upload.controller';
 import { FileController } from './file.controller';
 import { FileRepository } from './file.repository';
 import { FileService } from './file.service';
 import { UploadSessionRepository } from './upload-session.repository';
 import { UploadSessionService } from './upload-session.service';
-import { FileUploadController } from './file-upload.controller';
 
 @Module({
-  imports: [
-    FolderModule,
-    MulterModule.registerAsync({
-      inject: [MinioService],
-      useFactory: (minioService: MinioService) => ({
-        storage: new MinioStorageEngine(minioService),
-      }),
-    }),
-  ],
+  imports: [FolderModule, BullModule.registerQueue({ name: 'upload-session-cleanup' })],
   controllers: [FileController, FileDownloadController, FileUploadController],
   providers: [FileService, FileRepository, UploadSessionRepository, UploadSessionService],
-  exports: [FileService, UploadSessionRepository],
+  exports: [FileService],
 })
 export class FileModule {}
